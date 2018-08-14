70e12d35545c74d68a373d54079996fc2e71b968
Hubert Vaizand <hvaizand@prologis.com>
2018-08-14 14:45:00 -0600

    postGitActions

---
+++ vars/postGitActions.groovy
@@ -0,0 +1,6 @@
+def call(Map config) {
+    withCredentials([usernamePassword(credentialsId: config.credentials, passwordVariable: 'password', usernameVariable: 'username')]) {
+        pullRequest.addLabels(config.labels)
+        def comment = pullRequest.comment(config.comment)
+    }
+}
