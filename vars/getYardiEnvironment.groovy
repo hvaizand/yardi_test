d939806cac6d6e100c473b1244d2436df8418603
Hubert Vaizand <hvaizand@prologis.com>
2018-08-16 16:16:02 -0600

    getYardiEnvironment

---
+++ vars/getYardiEnvironment.groovy
@@ -0,0 +1,53 @@
+def call(){
+    //Check labels
+    def environment.module = 'unknown'
+    def environment.name = 'unknown'
+    def countmodule = 0
+    def countname = 0
+    for (label in pullRequest.labels) {
+        switch(label) {
+            case 'Core':
+                environment.module = 'unknown'
+                countmodule += 1
+            break
+            case 'ABF':
+                environment.module = 'unknown'
+                countmodule += 1
+            break
+            case 'Sandbox':
+                environment.name = 'sandbox'
+                countname += 1
+            break
+            case 'Development':
+                environment.name = 'dev'
+                countname += 1
+            break
+            case 'Beta':
+                environment.name = 'beta'
+                countname += 1
+            break
+            case 'Test':
+                environment.name = 'test'
+                countname += 1
+            break
+            case '2k16':
+                environment.name = '2k16'
+                countname += 1
+            break
+            case 'UAT':
+                environment.name = 'uat'
+                countname += 1
+            break
+        }
+    }
+    if(countmodule>1) {
+        environment.module = 'multiple'
+    }
+    if(countname>1) {
+        environment.name = 'multiple'
+    }
+    //Check target branch
+
+
+    return environment
+}
