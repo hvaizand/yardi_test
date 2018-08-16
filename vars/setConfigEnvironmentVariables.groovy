7b90f6e491e153dab6bacb11b974e2c76cbc4f28
Hubert Vaizand <hvaizand@prologis.com>
2018-08-16 12:10:46 -0600

    change

--- vars/setConfigEnvironmentVariables.groovy
+++ vars/setConfigEnvironmentVariables.groovy
@@ -1,19 +1,15 @@
-def call(LinkedHashMap config=null) {
+def call(LinkedHashMap config=null, file) {
   if (config == null) {
     try {
-      config = readYaml file: 'yardi_environments.yml'
+      config = readYaml file: "${file}"
     }
     catch (e) {
       echo "Environment variables skipped. Configuration not found."
       return
     }
   }
-  else if (fileExists("./Jenkinsfiles/yardi_environments.yml")) {
-      config = readYaml file: "./Jenkinsfiles/yardi_environments.yml"
-      echo "config ==> ${config}"
-  }
-  else {
-      config = []
+  for (var in config.environment) {
+    env[var.key] = var.value
   }
   return config
 }
