dc161c4b751934fac7ae3afc0f0255c57b9a0c74
Hubert Vaizand <hvaizand@prologis.com>
2018-08-24 16:40:17 -0600

    Var name

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,5 +1,5 @@
 def call(fileName){
-    withCredentials([usernamePassword(credentialsId: yardiconfig.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-            bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${yardiconfig.db_server} -d ${yardiconfig.db_name} -r1 -b -f 65001 -i ${fileName}"
+    withCredentials([usernamePassword(credentialsId: yardiConfig.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
+            bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${yardiConfig.db_server} -d ${yardiConfig.db_name} -r1 -b -f 65001 -i ${fileName}"
         }
 }
