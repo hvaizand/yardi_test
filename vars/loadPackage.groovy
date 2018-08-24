a722274997a632370e678e84d96ed1c26c0e00a3
Hubert Vaizand <hvaizand@prologis.com>
2018-08-24 15:09:46 -0600

    loadPackage

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,5 +1,5 @@
 def call(){
     withCredentials([usernamePassword(credentialsId: config.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-            bat encoding: 'UTF-8', returnStdout: true, script: "c:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
+            bat script: "c:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
         }
 }
