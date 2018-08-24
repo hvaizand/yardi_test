a2ca0b592a268239dbd7976268b3a2109fc837c2
Hubert Vaizand <hvaizand@prologis.com>
2018-08-24 16:12:00 -0600

    sqlcmd instead of pldpkgload

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,5 +1,5 @@
-def call(){
+def call(fileName){
     withCredentials([usernamePassword(credentialsId: config.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-            bat script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
+            bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i ${fileName}"
         }
 }
