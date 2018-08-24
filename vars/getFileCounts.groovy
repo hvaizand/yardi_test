f6f5bc0eac920996d3fa5bc76ea20984a012dff2
Hubert Vaizand <hvaizand@prologis.com>
2018-08-24 15:39:05 -0600

    New variable

--- vars/getFileCounts.groovy
+++ vars/getFileCounts.groovy
@@ -9,7 +9,7 @@ def call(countFiles){
         switch(ext) {
             case "pkg":
                 debugMessage "This is a package. The extension is ", ext
-                writeFile file: "${WORKSPACE}\\pldpkgload.pkglist", text: commitFile.filename
+                writeFile file: "${WORKSPACE}\\pldpkgload.pkglist", text: "${file}"
                 countFiles.countpkg += 1
             break
             case "txt":
