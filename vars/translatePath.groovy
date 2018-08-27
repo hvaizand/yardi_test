9cc0d538773446f3a1ae3777a879931191a1d877
Hubert Vaizand <hvaizand@prologis.com>
2018-08-27 16:53:32 -0600

    Var declaration

--- vars/translatePath.groovy
+++ vars/translatePath.groovy
@@ -1,13 +1,11 @@
  //private static Pattern regexDriveLetter = Pattern.compile("^[a-zA-Z]:");
  //private static Pattern regexUnc = Pattern.compile("^//");
- 
-def call (String pathname) {
-    Pattern regexSlashes = Pattern.compile("/");
-   
+ private static Pattern regexSlashes = Pattern.compile("/")  
+
+def call (String pathname) { 
     if (pathname == null) {
         return null;
-    }
-  
+    }  
 //  pathname = regexDriveLetter.matcher(pathname).replaceAll("");
     pathname = regexSlashes.matcher(pathname).replaceAll("\\\\");
 //  pathname = regexUnc.matcher(pathname).replaceAll("/mnt/");
