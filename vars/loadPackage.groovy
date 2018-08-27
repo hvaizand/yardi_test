db311df9f9467eee922b329769df2c4f135dd1d5
Hubert Vaizand <hvaizand@prologis.com>
2018-08-27 11:47:31 -0600

    Map

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,4 +1,4 @@
-def call(fileName, config){
+def call(fileName, LinkedHashMap config){
     withCredentials([usernamePassword(credentialsId: config.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
             bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${config.db_server} -d ${config.db_name} -r1 -b -f 65001 -i ${fileName}"
         }
