9d5305d859402a3a73f8ce7b455f120c14b9b814
Hubert Vaizand <hvaizand@prologis.com>
2018-08-27 11:14:46 -0600

    addLabels

--- vars/postGitActions.groovy
+++ vars/postGitActions.groovy
@@ -2,6 +2,7 @@ def call(Map config) {
     withCredentials([usernamePassword(credentialsId: config.credentials, passwordVariable: 'password', usernameVariable: 'username')]) {
 //        pullRequest.addLabels(config.labels)
         pullRequest.labels = config.listLabels
+        pullRequest.addLabels(config.labels)
         def comment = pullRequest.comment(config.comment)
     }
 }
