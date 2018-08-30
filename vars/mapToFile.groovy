be2df0beaa21ee476c373faa9867ec488116521a
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 16:06:51 -0600

    swap

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -1,7 +1,7 @@
 @NonCPS
 def call(listFiles, fileName) {
-    fileOperations([fileCreateOperation(fileContent: '', fileName: fileName)])
     echo "List of files: ${listFiles}"
+    fileOperations([fileCreateOperation(fileContent: '', fileName: fileName)])
     for(def item in listFiles){
         echo "File name: ${filName} - Value: ${item}\n"
         writeFile encoding: 'UTF-8', file: "${fileName}", text: "${item}\n"
