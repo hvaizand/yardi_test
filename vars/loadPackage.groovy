44cb7e07d5a36a7cb4854aa08df6dd24bd3cc889
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 10:38:32 -0600

    fileName and i

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -4,9 +4,9 @@
 def call(fileName, dbo_credentials, db_server, db_name){
     withCredentials([usernamePassword(credentialsId: dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
        for(int i = 0; i < fileName.size(); i++) {
-            echo "Load package: ${fileName.i}"
+            echo "Load package: " + fileName.[i]
 //            echo "Creds: ${dbo_credentials} - DB Server: ${db_server} - DB Name: ${db_name}"
-            bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i ${fileName.i}"
+//            bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i " + fileName.[i]
        }
     }
 }
