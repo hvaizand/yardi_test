c531ba33281c79ea40001cffe5ac58ae9c99c20a
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 15:34:22 -0600

    writeFile

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -4,7 +4,7 @@ def call(depmap, fileName) {
     fileOperations([fileCreateOperation(fileContent: '', fileName: fileName)])
     for (def entry2 in depmap) {
         dlist.add(new java.util.AbstractMap.SimpleImmutableEntry(entry2.key, entry2.value))
-        writeFile encoding: 'UTF-8', file: fileName, text: new java.util.AbstractMap.SimpleImmutableEntry(entry2.key, entry2.value)
+        writeFile encoding: 'UTF-8', file: fileName, text: entry2.value
     }
     dlist
 }
