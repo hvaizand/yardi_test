a7c6593cfe8dd48c65e7475c825f32cc28ed9348
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 11:38:45 -0600

    change variable

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -5,9 +5,9 @@ def call(fileName, dbo_credentials, db_server, db_name){
     withCredentials([usernamePassword(credentialsId: dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
         for (def e in mapToList(fileName)){
 //        for(int i = 0; i < fileName.size(); i++) {
-            echo "Load package: + ${e.value}"
+            echo "Load package: ${e.value}"
 //            echo "Creds: ${dbo_credentials} - DB Server: ${db_server} - DB Name: ${db_name}"
-            bat returnStdout: true, script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i " + fileName.[i]
+            bat returnStdout: true, script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i ${e.value}"
        }
     }
 }
