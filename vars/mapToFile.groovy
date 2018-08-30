8360e10f6f00674a2def2040076041c14184e7be
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 15:55:32 -0600

    typo

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -1,7 +1,7 @@
 @NonCPS
 def call(listFiles, fileName) {
     fileOperations([fileCreateOperation(fileContent: '', fileName: fileName)])
-    for(def e in listFiles)){
+    for(def e in listFiles){
         writeFile encoding: 'UTF-8', file: fileName, text: "${e.value}\n"
     }
 }
