7fa1c49349d71dca65275c1308f6e3c53afa0b1d
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 14:57:41 -0600

    create and populate pldpkload.pkglist file

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,13 +1,13 @@
 // def call(body){
 //     bat script: body
 // }
-def call(fileName, dbo_credentials, db_server, db_name){
+def call(fileList, dbo_credentials, db_server, db_name){
     withCredentials([usernamePassword(credentialsId: dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-        for (def e in mapToList(fileName)){
-//        for(int i = 0; i < fileName.size(); i++) {
-            echo "Load package: ${e.value}"
-//            echo "Creds: ${dbo_credentials} - DB Server: ${db_server} - DB Name: ${db_name}"
-            bat returnStdout: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${e.value}\""
-       }
+//        for (def e in mapToFile(fileList, env.WORKSPACE'\\pldpkgload.pkglist')){
+//        for(int i = 0; i < fileList.size(); i++) {
+            def e = mapToFile(fileList, env.WORKSPACE'\\pldpkgload.pkglist')
+            echo "Load ${e.size()} package(s)"
+            bat returnStdout: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
+//       }
     }
 }
---
+++ vars/mapToFile.groovy
@@ -0,0 +1,11 @@
+@NonCPS
+def call(depmap, fileName) {
+    def dlist = []
+    fileOperations([fileCreateOperation(fileContent: '', fileName: fileName)])
+    for (def entry2 in depmap) {
+        dlist.add(new java.util.AbstractMap.SimpleImmutableEntry(entry2.key, entry2.value))
+        writeFile encoding: 'UTF-8', file: fileName, text: new java.util.AbstractMap.SimpleImmutableEntry(entry2.key, entry2.value)
+    }
+    dlist
+}
+//, fileCopyOperation(excludes: '', flattenFiles: false, includes: 'pldpkgload.pkglist', targetLocation: '')
