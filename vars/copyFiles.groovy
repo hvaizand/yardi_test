eac6ae73291a488251ea423c6781018be2c95eec
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 11:30:30 -0600

    utils

--- vars/copyFiles.groovy
+++ vars/copyFiles.groovy
@@ -1,11 +1,8 @@
 def call(fileName, targetPath, rdp_credentials){
     withCredentials([usernamePassword(credentialsId: rdp_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-    //    for(int i = 0; i < fileName.size(); i++) {
-    //         echo "Copy report: ${fileName.[i]}"
-        for (def e in mapToList(fileName)){
+        for (def e in utils.mapToList(fileName)){
             echo "Copy report: + ${e.value}"
-//            echo "Creds: ${dbo_credentials} - DB Server: ${db_server} - DB Name: ${db_name}"
-//            bat script: "sqlcmd -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i ${fileName}"
+
        }
     }        
 }
