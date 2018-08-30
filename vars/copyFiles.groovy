7ed44db0b95c7cc8224d6260ddd8750bd9fb4b00
Hubert Vaizand <hvaizand@prologis.com>
2018-08-30 11:33:16 -0600

    remove utils

--- vars/copyFiles.groovy
+++ vars/copyFiles.groovy
@@ -1,6 +1,6 @@
 def call(fileName, targetPath, rdp_credentials){
     withCredentials([usernamePassword(credentialsId: rdp_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-        for (def e in utils.mapToList(fileName)){
+        for (def e in mapToList(fileName)){
             echo "Copy report: + ${e.value}"
 
        }
