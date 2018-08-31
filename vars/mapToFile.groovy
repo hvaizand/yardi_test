d1239a8fb627a2ffdc57136bec61d73378cc6cf7
Hubert Vaizand <hvaizand@prologis.com>
2018-08-31 10:42:52 -0600

    change

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -2,13 +2,14 @@
 def call(listFiles, fileName) {
     echo "List of files: ${listFiles}"
     def listFilesString
-    def filePath
+//    def filePath
     for(def item in listFiles){
         echo "File name: ${fileName} - Value: ${item}\n"
 //        item.substring(commitFile.filename.lastIndexOf('=') + 1, item.length())
-        filePath = item.substring(item.lastIndexOf('=') + 1, item.length())
-        echo "File path: ${filePath}"
-        listFilesString += "${filePath}\n"
+//        filePath = item.substring(item.lastIndexOf('=') + 1, item.length())
+//        echo "File path: ${filePath}"
+//        listFilesString += "${filePath}\n"
+        listFilesString += "${item}\n"
         echo "List file string: ${listFilesString}"
     }
     writeFile encoding: 'UTF-8', file: "${fileName}", text: "${listFileString}"
