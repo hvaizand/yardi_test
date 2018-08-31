c8b887a6a28e6eddb1354a295c05ff79fafd8cd7
Hubert Vaizand <hvaizand@prologis.com>
2018-08-31 10:32:37 -0600

    extract file name and add to string Then write to file

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -1,9 +1,12 @@
 @NonCPS
 def call(listFiles, fileName) {
     echo "List of files: ${listFiles}"
+    def listFilesString
     for(def item in listFiles){
         echo "File name: ${fileName} - Value: ${item}\n"
-        writeFile encoding: 'UTF-8', file: "${fileName}", text: "${item}\n"
+//        item.substring(commitFile.filename.lastIndexOf('=') + 1, item.length())
+        listFilesString += "${item.substring(commitFile.filename.lastIndexOf('=') + 1, item.length())}\n"
     }
+    writeFile encoding: 'UTF-8', file: "${fileName}", text: "${listFileString}"
 //    return listFiles
 }
