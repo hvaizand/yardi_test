40420535b98a986007427e4db920126b947eb03a
Hubert Vaizand <hvaizand@prologis.com>
2018-08-31 10:41:34 -0600

    change entry in list

--- vars/mapToList.groovy
+++ vars/mapToList.groovy
@@ -2,7 +2,7 @@
 def call(depmap) {
     def dlist = []
     for (def entry2 in depmap) {
-        dlist.add(new java.util.AbstractMap.SimpleImmutableEntry(entry2.key, entry2.value))
+        dlist.add(entry2.value)
     }
     dlist
 }
