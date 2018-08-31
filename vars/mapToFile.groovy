e538f10bb170b724ec097311ab486b9b3b4882cc
Hubert Vaizand <hvaizand@prologis.com>
2018-08-31 10:34:09 -0600

    typo

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -5,7 +5,7 @@ def call(listFiles, fileName) {
     for(def item in listFiles){
         echo "File name: ${fileName} - Value: ${item}\n"
 //        item.substring(commitFile.filename.lastIndexOf('=') + 1, item.length())
-        listFilesString += "${item.substring(commitFile.filename.lastIndexOf('=') + 1, item.length())}\n"
+        listFilesString += "${item.substring(item.lastIndexOf('=') + 1, item.length())}\n"
     }
     writeFile encoding: 'UTF-8', file: "${fileName}", text: "${listFileString}"
 //    return listFiles
