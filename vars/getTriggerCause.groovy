e9bfc7af47e99dda060d4261e5dceafd919743bd
Hubert Vaizand <hvaizand@prologis.com>
2018-09-04 13:36:39 -0600

    change echo

--- vars/getTriggerCause.groovy
+++ vars/getTriggerCause.groovy
@@ -3,12 +3,10 @@ def call(){
 //    def causes = currentBuild.rawBuild.getCauses()
 //    echo "Cause: ${causes}"
     def triggerCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
-    echo "${triggerCause.userLogin} - ${triggerCause.comment} - ${triggerCause.triggerPattern}"
+//    echo "${triggerCause.userLogin} - ${triggerCause.comment} - ${triggerCause.triggerPattern}"
     def cause = ''
     if (triggerCause) {
-        cause = "Build was started by ${triggerCause.userLogin}, who wrote: " +
-            "\"${triggerCause.comment}\", which matches the " +
-            "\"${triggerCause.triggerPattern}\" trigger pattern."
+        cause = "Build was started by ${triggerCause.userLogin}, who wrote: \"${triggerCause.comment}\", which matches the \"${triggerCause.triggerPattern}\" trigger pattern."
     } else {
         cause = 'Build was not started by a trigger'
     }
