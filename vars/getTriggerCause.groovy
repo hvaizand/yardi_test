3fdb40794ac494038c6cd256aceb27e43eda5620
Hubert Vaizand <hvaizand@prologis.com>
2018-09-05 11:18:11 -0600

    causes

--- vars/getTriggerCause.groovy
+++ vars/getTriggerCause.groovy
@@ -1,8 +1,8 @@
 @NonCPS
 def call(){
-//    def causes = currentBuild.rawBuild.getCauses()
-//    echo "Cause: ${causes}"
-    def triggerCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
+    def causes = currentBuild.rawBuild.getCauses(hudson.model.Cause$UserIdCause)
+    echo "Cause: ${causes}"
+//    def triggerCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
 //    echo "${triggerCause.userLogin} - ${triggerCause.comment} - ${triggerCause.triggerPattern}"
-    return triggerCause
+    return causes
 }
