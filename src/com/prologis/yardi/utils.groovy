57f53a843684de0236fb5108e3ac65099d18a8d2
Hubert Vaizand <hvaizand@prologis.com>
2018-09-05 15:08:06 -0600

    mapDrive unmapDrive

--- src/com/prologis/yardi/utils.groovy
+++ src/com/prologis/yardi/utils.groovy
@@ -9,3 +9,14 @@ def mapToList(depmap) {
     }
     dlist
 }
+
+def mapDrive(config){
+    withCredentials([usernamePassword(credentialsId: params.RdpUser, passwordVariable: 'RDPPASSWORD', usernameVariable: 'RDPUSERID')]) {
+        bat returnStdout: true, script: "NET USE ${config.drive_letter} \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
+    }
+}
+
+def unmapDrive(config){
+        bat returnStdout: true, script: "NET USE * /del /y"
+    }
+}
