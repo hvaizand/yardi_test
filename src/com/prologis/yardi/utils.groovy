79473dd96780f95098e6d0845b367edaa54aa6c9
Hubert Vaizand <hvaizand@prologis.com>
2018-09-06 11:38:43 -0600

    returnStatus

--- src/com/prologis/yardi/utils.groovy
+++ src/com/prologis/yardi/utils.groovy
@@ -33,11 +33,19 @@ def mapToFile(listFiles, fileName) {
 def mapDrive(config){
     withCredentials([usernamePassword(credentialsId: params.RdpUser, passwordVariable: 'RDPPASSWORD', usernameVariable: 'RDPUSERID')]) {
         echo "NET USE ${config.drive_letter} \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
-        bat returnStdout: true, script: "NET USE ${config.drive_letter}: \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
+        def mapStatus = bat returnStatus: true, script: "NET USE ${config.drive_letter}: \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
+    if(mapStatus!=0){
+        currentBuild.result = 'UNSTABLE'
+        notification.message = "An issue occurred when trying to map the network drive pointing to the default path. Please update your RDP password in Jenkins"
+    }
     }
 }
 
 // Unmap netwrok drive
 def unmapDrive(){
-    bat returnStdout: true, script: "NET USE * /del /y"
+    def mapStatus = bat returnStatus: true, script: "NET USE * /del /y"
+    if(mapStatus!=0){
+        currentBuild.result = 'UNSTABLE'
+        notification.message = "An issue occurred when trying to unmap the network drive pointing to the default path. Contact your admin"
+    }
 }
