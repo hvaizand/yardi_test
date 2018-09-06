c6f0ab59e6363108bfd8a384bd5553f65ff10d14
Hubert Vaizand <hvaizand@prologis.com>
2018-09-06 10:59:57 -0600

    use utils

--- src/com/prologis/yardi/utils.groovy
+++ src/com/prologis/yardi/utils.groovy
@@ -13,6 +13,22 @@ def mapToList(depmap) {
     dlist
 }
 
+//Create file with content of groovy list
+@NonCPS
+def mapToFile(listFiles, fileName) {
+    debugMessage "mapToFile - List of files", "${listFiles}"
+    def listFilesString = ''
+    def filePath
+    for(def entry2 in listFiles){
+        debugMessage "mapToFile - entry2", "${entry2}"
+        filePath = "${entry2}".substring("${entry2}".indexOf('=') + 1, "${entry2}".length())
+        debugMessage "mapToFile - File path:", "${filePath}"
+        listFilesString += "${filePath}\n"
+        debugMessage "mapToFile - List file string:", "${listFilesString}"
+    }
+    writeFile encoding: 'UTF-8', file: "${fileName}", text: "${listFilesString}"
+}
+
 // Map network drive
 def mapDrive(config){
     withCredentials([usernamePassword(credentialsId: params.RdpUser, passwordVariable: 'RDPPASSWORD', usernameVariable: 'RDPUSERID')]) {
--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,8 +1,8 @@
 def call(fileList, dbo_credentials, db_server, db_name){
     withCredentials([usernamePassword(credentialsId: dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-            debugMessage "List of packages:", "${fileList}"
-            createFile "pldpkgload.pkglist"
-            mapToFile(fileList, "pldpkgload.pkglist")
+            debugMessage "loadPackage - List of packages:", "${fileList}"
+            utils.createFile "pldpkgload.pkglist"
+            utils.mapToFile(fileList, "pldpkgload.pkglist")
             bat returnStdout: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
     }
 }
