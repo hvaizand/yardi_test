5b48c660e95f51a979ac32e9308e1160c35c7e2f
Hubert Vaizand <hvaizand@prologis.com>
2018-09-07 16:28:25 -0600

    using BAT

--- vars/copyFiles.groovy
+++ vars/copyFiles.groovy
@@ -1,6 +1,15 @@
 def call(fileName, config){
+    def message = ''
     for (def e in mapToList(fileName)){
         echo "Copy report: ${e.value} to ${config.drive_letter}:\\"
-        fileOperations([fileCopyOperation(excludes: '', flattenFiles: false, includes: "${e.value}", targetLocation: "${config.drive_letter}:\\")])
+//        fileOperations([fileCopyOperation(excludes: '', flattenFiles: false, includes: "${e.value}", targetLocation: "${config.drive_letter}:\\")])
+        def copyStatus = bat returnStatus: true, script: "COPY \"${e.value}\" ${config.drive_letter}:\\ /Y /Z"
+        if(copyStatus!=0){
+            currentBuild.result = 'FAILURE'
+            message = "An issue occurred when copying the report files. Please review the logs"
+        } else {
+            debugMessage "copyFile - copy status", copyStatus
+            message = "Successful"
+        }
     }        
 }
