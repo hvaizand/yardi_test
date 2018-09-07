a17870adc473cfdc41dc9efc3806680809ea34fb
Hubert Vaizand <hvaizand@prologis.com>
2018-09-07 16:19:31 -0600

    comment debug

--- vars/mapToFile.groovy
+++ vars/mapToFile.groovy
@@ -7,9 +7,9 @@ def call(listFiles, fileName) {
     for(def entry2 in listFiles){
         filePath = "${entry2}".substring("${entry2}".indexOf('=') + 1, "${entry2}".length())
         listFilesString += "${filePath}\n"
-        debugMessage "mapToFile - entry2", "${entry2}"
-        debugMessage "mapToFile - File path:", "${filePath}"
-        debugMessage "mapToFile - List file string:", "${listFilesString}"
+//        debugMessage "mapToFile - entry2", "${entry2}"
+//        debugMessage "mapToFile - File path:", "${filePath}"
+//        debugMessage "mapToFile - List file string:", "${listFilesString}"
     }
     writeFile encoding: 'UTF-8', file: "${fileName}", text: "${listFilesString}"
 }
