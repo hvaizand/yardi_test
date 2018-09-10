e3edf5bd65926aabaeb4852e80fe28f97bb98fd2
Hubert Vaizand <hvaizand@prologis.com>
2018-09-10 14:01:55 -0600

    use vars instead of src

--- vars/mapDrive.groovy
+++ vars/mapDrive.groovy
@@ -1,5 +1,15 @@
 def call(config){
+    def message = ''
     withCredentials([usernamePassword(credentialsId: params.RdpUser, passwordVariable: 'RDPPASSWORD', usernameVariable: 'RDPUSERID')]) {
-        bat returnStdout: true, script: "NET USE ${config.drive_letter} \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
+        echo "NET USE ${config.drive_letter}: \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
+        def mapStatus = bat returnStatus: true, script: "NET USE ${config.drive_letter}: \"${config.def_path}\" \"${RDPPASSWORD}\" /USER:${RDPUSERID} /y"
+        if(mapStatus!=0){
+            currentBuild.result = 'UNSTABLE'
+            notification.message = "An issue occurred when trying to map the network drive pointing to the default path. Please update your RDP password in Jenkins"
+        } else {
+            debugMessage "utils.mapDrive - Map network drive", mapStatus
+            notification.message = "Successful"
+        }
     }
+    return mapStatus
 }
