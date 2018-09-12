06bbfda44f1badfd350c709f898dd6ee4ab7cb80
Hubert Vaizand <hvaizand@prologis.com>
2018-09-12 14:06:50 -0600

    handlePipelineStepErrors

---
+++ vars/handlePipelineStepErrors.groovy
@@ -0,0 +1,46 @@
+
+def call(Map parameters = [:], body) {
+
+    def stepParameters = parameters.stepParameters //mandatory
+    def stepName = parameters.stepName //mandatory
+    def echoDetails = parameters.get('echoDetails', true)
+
+    try {
+
+        if (stepParameters == null && stepName == null)
+            error "step handlePipelineStepErrors requires following mandatory parameters: stepParameters, stepName"
+
+        if (echoDetails)
+            echo "--- BEGIN LIBRARY STEP: ${stepName}.groovy ---"
+
+        body()
+
+    } catch (Throwable err) {
+        if (echoDetails)
+            echo """----------------------------------------------------------
+--- ERROR OCCURED IN LIBRARY STEP: ${stepName}
+----------------------------------------------------------
+
+FOLLOWING PARAMETERS WERE AVAILABLE TO THIS STEP:
+***
+${stepParameters}
+***
+
+ERROR WAS:
+***
+${err}
+***
+
+FURTHER INFORMATION:
+* Documentation of library step ${stepName}: https://sap.github.io/jenkins-library/steps/${stepName}/
+* Source code of library step ${stepName}: https://github.com/SAP/jenkins-library/blob/master/vars/${stepName}.groovy
+* Library documentation: https://sap.github.io/jenkins-library/
+* Library repository: https://github.com/SAP/jenkins-library
+ 
+----------------------------------------------------------"""
+        throw err
+    } finally {
+        if (echoDetails)
+            echo "--- END LIBRARY STEP: ${stepName}.groovy ---"
+    }
+}
--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,17 +1,42 @@
-def call(fileList, dbo_credentials, db_server, db_name){
-    def message = ''
-    withCredentials([usernamePassword(credentialsId: dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
-            debugMessage "loadPackage - List of packages:", "${fileList}"
+import groovy.transform.Field
+
+@Field def STEP_NAME = 'loadPackage'
+
+def call(Map parameters = [:]) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+        def message = ''
+        withCredentials([usernamePassword(credentialsId: parameters.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
+            debugMessage "loadPackage - List of packages:", "${parameters.fileList}"
             createFile "pldpkgload.pkglist"
-            mapToFile(fileList, "pldpkgload.pkglist")
-            def loadStatus = bat returnStdout: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
-            if(loadStatus!=0){
+            mapToFile(parameters.fileList, "pldpkgload.pkglist")
+            def returnStatus = bat returnStatus: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${parameters.db_server} -d ${parameters.db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
+            if(returnStatus!=0){
                 currentBuild.result = 'FAILURE'
                 message = "An issue occurred when trying to load packages. Please review the logs"
             } else {
-                debugMessage "loadPackage - pldpkgload", loadStatus
+                debugMessage "loadPackage - pldpkgload", returnStatus
                 message = "Successful"
             }
+        }
+        return message
     }
-    return message
 }
+
+
+// def call(fileList, dbo_credentials, db_server, db_name){
+//     def message = ''
+//     withCredentials([usernamePassword(credentialsId: dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
+//             debugMessage "loadPackage - List of packages:", "${fileList}"
+//             createFile "pldpkgload.pkglist"
+//             mapToFile(fileList, "pldpkgload.pkglist")
+//             def loadStatus = bat returnStatus: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${db_server} -d ${db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
+//             if(loadStatus!=0){
+//                 currentBuild.result = 'FAILURE'
+//                 message = "An issue occurred when trying to load packages. Please review the logs"
+//             } else {
+//                 debugMessage "loadPackage - pldpkgload", loadStatus
+//                 message = "Successful"
+//             }
+//     }
+//     return message
+// }
