ae7a5e35e50e881d0e4d9023f28890a0cf7c626e
Hubert Vaizand <hvaizand@prologis.com>
2018-09-12 16:45:38 -0600

    Add handlePipelineStepErrors

--- vars/copyFiles.groovy
+++ vars/copyFiles.groovy
@@ -1,15 +1,17 @@
-def call(fileName, config){
-    def message = ''
-    for (def e in mapToList(fileName)){
-        echo "Copy report: ${e.value} to ${config.drive_letter}:\\"
-//        fileOperations([fileCopyOperation(excludes: '', flattenFiles: false, includes: "${e.value}", targetLocation: "${config.drive_letter}:\\")])
-        def copyStatus = bat returnStatus: true, script: "COPY \"${e.value}\" ${config.drive_letter}:\\ /Y /Z"
-        if(copyStatus!=0){
-            currentBuild.result = 'FAILURE'
-            message = "An issue occurred when copying the report files. Please review the logs"
-        } else {
-            debugMessage "copyFile - copy status", copyStatus
-            message = "Successful"
-        }
-    }        
+def call(Map parameters = [:]) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+        def message = ''
+        for (def e in mapToList(parameters.fileNames)){
+            echo "Copy report: ${e.value} to ${parameters.targetDrive}:\\"
+            def returnStatus = bat returnStatus: true, script: "COPY \"${e.value}\" ${parameters.targetDrive}:\\ /Y /Z"
+            if(returnStatus!=0){
+                currentBuild.result = 'FAILURE'
+                message = "An issue occurred when copying the report files. Please review the logs"
+            } else {
+                debugMessage "copyFile - copy status", returnStatus
+                message = "Successful"
+            }
+        }      
+        return message  
+    }
 }
