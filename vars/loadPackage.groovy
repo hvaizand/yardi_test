c8fd16bb824ee14658d757e6096fa90a183cc8f9
Hubert Vaizand <hvaizand@prologis.com>
2018-09-13 10:56:39 -0600

    clean up

--- vars/loadPackage.groovy
+++ vars/loadPackage.groovy
@@ -1,31 +1,29 @@
 import groovy.transform.Field
 
+import org.jenkinsci.plugins.workflow.steps.MissingContextVariableException
+
 @Field def STEP_NAME = 'loadPackage'
 @Field Set STEP_CONFIG_KEYS = [
-    'dbo_credentials',
-    'db_server',
-    'db_name',
     'pldpkgload'
 ]
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
 
 def call(Map parameters = [:]) {
     handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
-        def message = ''
+        
+        echo "Loading Packages - ${parameters.count} packages"
         withCredentials([usernamePassword(credentialsId: parameters.dbo_credentials, passwordVariable: 'DBPASSWORD', usernameVariable: 'DBUSERNAME')]) {
+            
             debugMessage "loadPackage - List of packages:", "${parameters.filePackage}"
+            
             createFile "pldpkgload.pkglist"
+            
             mapToFile(parameters.filePackage, "pldpkgload.pkglist")
-            def returnStatus = bat returnStatus: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${parameters.db_server} -d ${parameters.db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\""
-            if(returnStatus!=0){
-                currentBuild.result = 'FAILURE'
-                message = "An issue occurred when trying to load packages. Please review the logs"
-            } else {
-                debugMessage "loadPackage - pldpkgload", returnStatus
-                message = "Successful"
+            
+            if (bat(returnStatus: true, script: "C:\\Utils\\pldpkgload.exe -U ${DBUSERNAME} -P ${DBPASSWORD} -S ${parameters.db_server} -d ${parameters.db_name} -r1 -b -f 65001 -i \"${WORKSPACE}\\pldpkgload.pkglist\"") != 0) {
+                error "[${STEP_NAME}] An issue occurred when trying to load packages. Please review the logs - aborting ${STEP_NAME}"
             }
         }
-        return message
     }
 }
 
