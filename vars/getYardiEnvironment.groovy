93680e4e2529f11e3531e5d8f20dc5bedc37f2e1
Hubert Vaizand <hvaizand@prologis.com>
2019-08-27 12:14:37 +0200

    Remove only specific labels

--- vars/getYardiEnvironment.groovy
+++ vars/getYardiEnvironment.groovy
@@ -7,25 +7,34 @@ def call(environment){
             case 'Environment: Sandbox':
                 environment.name = 'sandbox'
                 environment.deploysbx = 'true'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: sbx') }
                 countname += 1
             break
             case 'Environment: Development':
                 environment.name = 'dev'
                 environment.deploydev = 'true'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: dev') }
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: tst') }
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: uat') }
                 countname += 1
             break
             case 'Environment: Beta':
                 environment.name = 'beta'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: beta') }
                 countname += 1
             break
             case 'Environment: Test':
                 environment.name = 'dev-test'
                 environment.deploydev = 'true'
                 environment.deploytst = 'true'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: dev') }
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: tst') }
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: uat') }
                 countname += 1
             break
             case 'Environment: 2k16':
                 environment.name = '2k16'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: 2k16') }
                 countname += 1
             break
             case 'Environment: UAT':
@@ -33,10 +42,14 @@ def call(environment){
                 environment.deploydev = 'true'
                 environment.deploytst = 'true'
                 environment.deployuat = 'true'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: dev') }
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: tst') }
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: uat') }
                 countname += 1
             break
             case 'Environment: Upgrade':
                 environment.name = 'upg'
+                environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed: upg') }
                 countname += 1
             break
         }
@@ -54,8 +67,10 @@ def call(environment){
                 case 'CI: Force':
                     environment.deployType = 'force'
                 break
+                default:
+                    environment.deployType = 'skip'
+                break
             }
-            environment.listLabels.removeAll { it.toLowerCase().startsWith('deployed:') }
             environment.listLabels.removeAll { it.toLowerCase().startsWith('environment:') }
         }
         environment.listLabels.add(label)
