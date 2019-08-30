81620731c0b8096b738b75b3fea750a401958f8d
Hubert Vaizand <hvaizand@prologis.com>
2019-08-30 14:25:55 +0200

    Test new return

--- vars/doBuild.groovy
+++ vars/doBuild.groovy
@@ -4,21 +4,22 @@ import groovy.transform.Field
 @Field Set STEP_CONFIG_KEYS = [
     'fileType',
     'module',
-    'fileCount'
+    'fileCount',
+    'fileList'
 ]
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
 
 def call(Map parameters = [:]) {
     handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
         if(parameters.count!=0) {
-            def fileList = [:]
-            fileList = checkFiles (fileType: parameters.fileType, environmentType: parameters.module)
-            parameters.fileCount = fileList.size()
+            //def fileList = [:]
+            parameters.fileList = checkFiles (fileType: parameters.fileType, environmentType: parameters.module)
+            parameters.fileCount = parameters.fileList.size()
 
-            debugMessage "doBuild - List ${parameters.fileType}", "${fileList}"
+            debugMessage "doBuild - List ${parameters.fileType}", "${parameters.fileList}"
             debugMessage "doBuild - Count ${parameters.fileType}", "${parameters.fileCount}"
-
-            return fileList
+    
+            //return parameters.fileList
         }
     }
 }
